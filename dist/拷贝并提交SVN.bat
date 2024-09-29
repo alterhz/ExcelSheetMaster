447b6d8d42ad31e24e2e86f6aa4fda80944fb157
550360139 <550360139@qq.com>
2024-09-29 10:34:41 +0800

    路径

--- dist/拷贝并提交SVN.bat
+++ dist/拷贝并提交SVN.bat
@@ -1,5 +1,5 @@
 @echo off
-copy /y "E:\src\github\ExcelSheetMaster\dist\main.exe" "E:\FW2\tools\ExcelSheetMaster\main.exe"
+copy /y "E:\src\github\ExcelSheetMaster\dist\ESheetSearchMaster.exe" "E:\FW2\tools\ExcelSheetMaster\ESheetSearchMaster.exe"
 cd /d "E:\FW2\tools\ExcelSheetMaster"
 explorer "E:\FW2\tools\ExcelSheetMaster"
 TortoiseProc.exe /command:commit /path:. /closeonend:0
